c8fcdd632dccfd1ea6f6e70c554c35252776f7de
jocelinkisenga <kisengajocelin7@gmail.com>
2022-07-09 00:04:17 +0200

    ajout des dependances

--- src/Test.php
+++ src/Test.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace Lushidev\ApiLocation;
+use GuzzleHttp\Client;
 
 class Test {
-
-	public function index ($name){
-		echo $name;
+		public $ip;
+	public function index (){
+	$this->ip = '134.201.250.155';
+        $client = new Client([
+             'base_uri'  => getenv('API_URL_LOC'),
+              'headers'   =>  [
+                    $this->ip,
+                    'access_key' => getenv('APILOC_KEY')
+                ]
+            ]);
+        /*return response()->json($client);*/
+        return json_decode($client->getBody());
 	}
 }
